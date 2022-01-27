96fd16a4c79e20ae04e8f1dcff67fa46522a90fa
Diogo M Shima <acdhp@yahoo.com.br>
2022-01-27 14:08:00 -0300

    First commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package com.dio;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		int nascimento = 1997;
+		int anoAtual = 2022;
+		int idade = anoAtual - nascimento;
+		System.out.println("Diogo é o cara! E esse ano ele faz " + idade + " anos!");
+		
+	}
+
+}
---
+++ src/com/dio/pets/Gato.java
@@ -0,0 +1,16 @@
+package com.dio.pets;
+
+public class Gato {
+	
+	
+	public static void main(String[] args) {
+	
+		String cor = "preto";
+		String nome = "Skitz";
+		Integer idade = 2;
+		
+		System.out.println("O gato " + cor + " tem " + idade + " anos de idade, e se chama " + nome);
+;
+	}	
+	
+}
